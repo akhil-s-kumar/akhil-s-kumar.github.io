babdfad36dd6230ef9429d54118acf4ab89ebab3
Akhil <akhilskumar120@gmail.com>
2022-12-11 22:12:35 +0530

    Added projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,14 +3,14 @@ const projects = [
     title: "Join Indian Forces",
     description: "Complete LMS for defence aspirants",
     sourceLink: "",
-    liveLink: "",
+    liveLink: "http://joinindianforces.in/",
     techStacks: [{ stack: "React" }, { stack: "GraphQL" }, { stack: "Django" }]
   },
   {
     title: "Apni Career",
     description: "Easiest tool to find your perfect job",
-    sourceLink: "",
-    liveLink: "",
+    sourceLink: "https://github.com/akhil-s-kumar/apnicareer-webapp",
+    liveLink: "https://www.apnicareer.com/",
     techStacks: [{ stack: "React" }, { stack: "GraphQL" }, { stack: "Django" }]
   },
   {
@@ -24,8 +24,22 @@ const projects = [
     title: "Covid-19 Stats",
     description: "Tool to find complete stats and vaccination availability",
     sourceLink: "https://github.com/akhil-s-kumar/COVID-19",
-    liveLink: "",
+    liveLink: "https://akhil-s-kumar.github.io/COVID-19/",
     techStacks: [{ stack: "React" }, { stack: "GraphQL" }, { stack: "Django" }]
+  },
+  {
+    title: "Tesla Stock Prediction",
+    description: "ML program to predict closing price of tesla stocks",
+    sourceLink: "https://github.com/akhil-s-kumar/Tesla-Stock-Prediction",
+    liveLink: "",
+    techStacks: [{ stack: "Scikit Learn" }, { stack: "Flask" }, { stack: "HTML" }]
+  },
+  {
+    title: "Car Price Prediction",
+    description: "ML program to predict price of a car based on various features",
+    sourceLink: "https://github.com/akhil-s-kumar/Car-Price-prediction",
+    liveLink: "",
+    techStacks: [{ stack: "Scikit Learn" }, { stack: "Flask" }, { stack: "HTML" }]
   },
 ]
 
